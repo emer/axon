b84b069ec5519a6e4e15cbe051024fa0fe0ec148
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2021-05-11 02:05:36 -0700

    v1.2.29 release -- version.go updated

--- axon/version.go
+++ axon/version.go
@@ -3,8 +3,8 @@
 package axon
 
 const (
-	Version     = "v1.2.28"
-	GitCommit   = "0c9fd43" // the commit JUST BEFORE the release
-	VersionDate = "2021-05-11 02:55" // UTC
+	Version     = "v1.2.29"
+	GitCommit   = "12b9ee0" // the commit JUST BEFORE the release
+	VersionDate = "2021-05-11 09:05" // UTC
 )
 
