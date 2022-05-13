8ce9f6e625ef3848e544765a6044c97bbbe0110e
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2022-05-13 15:33:18 -0700

    Add simple agent to axon examples

---
+++ axon/helper_methods.go
@@ -0,0 +1,265 @@
+package axon
+
+import (
+	"fmt"
+	"github.com/emer/emergent/agent"
+	"github.com/emer/emergent/egui"
+	"github.com/emer/emergent/elog"
+	"github.com/emer/emergent/emer"
+	"github.com/emer/emergent/etime"
+	"github.com/emer/emergent/looper"
+	"github.com/emer/emergent/params"
+	"github.com/emer/etable/agg"
+	"github.com/emer/etable/etensor"
+	"github.com/goki/gi/gi"
+	"time"
+)
+
+// AddDefaultLoopSimLogic adds some sim related logic to looper.Manager. It makes some assumptions about how the loop stack is set up which may cause it to fail.
+func AddDefaultLoopSimLogic(manager *looper.Manager, time *Time, net *Network) {
+	// Net Cycle
+	for m, _ := range manager.Stacks {
+		manager.Stacks[m].Loops[etime.Cycle].Main.Add("Axon:Cycle:RunAndIncrement", func() {
+			net.Cycle(time)
+			time.CycleInc()
+		})
+	}
+	// Weight updates.
+	// Note that the substring "UpdateNetView" in the name is important here, because it's checked in AddDefaultGUICallbacks.
+	manager.GetLoop(etime.Train, etime.Trial).OnEnd.Add("Axon:LoopSegment:UpdateWeights", func() {
+		net.DWt(time)
+		// TODO Need to update net view here to accurately display weight changes.
+		net.WtFmDWt(time)
+	})
+
+	// Set variables on ss that are referenced elsewhere, such as ApplyInputs.
+	for m, loops := range manager.Stacks {
+		curMode := m // For closures.
+		for t, loop := range loops.Loops {
+			curTime := t
+			loop.OnStart.Add(curMode.String()+":"+curTime.String()+":"+"SetTimeVal", func() {
+				time.Mode = curMode.String()
+			})
+		}
+	}
+}
+
+// SendActionAndStep takes action for this step, using either decoded cortical
+// or reflexive subcortical action from env.
+func SendActionAndStep(net *Network, ev agent.WorldInterface) {
+	// Get the first Target (output) layer
+	actions := map[string]agent.Action{}
+	for _, lnm := range net.LayersByClass(emer.Target.String()) {
+		ly := net.LayerByName(lnm).(AxonLayer).AsAxon()
+		vt := &etensor.Float32{}
+		ly.UnitValsTensor(vt, "ActM")
+		actions[lnm] = agent.Action{Vector: vt}
+	}
+	_, debug := ev.StepWorld(actions, false)
+	if debug != "" {
+		fmt.Println("Got debug from Step: " + debug)
+	}
+}
+
+// ToggleLayersOff can be used to disable layers in a Network, for example if you are doing an ablation study.
+func ToggleLayersOff(net *Network, layerNames []string, off bool) {
+	for _, lnm := range layerNames {
+		lyi := net.LayerByName(lnm)
+		if lyi == nil {
+			fmt.Printf("layer not found: %s\n", lnm)
+			continue
+		}
+		lyi.SetOff(off)
+	}
+}
+
+// ApplyInputs applies input patterns from given environment.
+// It is good practice to have this be a separate method with appropriate
+// args so that it can be used for various different contexts
+// (training, testing, etc).
+func ApplyInputs(net *Network, en agent.WorldInterface, layer string, patfunc func(spec agent.SpaceSpec) etensor.Tensor) {
+	lyi := net.LayerByName(layer)
+	lyi.(AxonLayer).InitExt() // Clear any existing inputs
+	if lyi == nil {
+		return
+	}
+	ly := lyi.(AxonLayer).AsAxon()
+	ss := agent.SpaceSpec{ContinuousShape: lyi.Shape().Shp, Stride: lyi.Shape().Strd}
+	pats := patfunc(ss)
+	if pats != nil {
+		ly.ApplyExt(pats)
+	}
+}
+
+// SaveWeights saves the network weights -- when called with giv.CallMethod
+// it will auto-prompt for filename
+func SaveWeights(fileName string, net *Network) {
+	fnm := fileName
+	fmt.Printf("Saving Weights to: %v\n", fnm)
+	net.SaveWtsJSON(gi.FileName(fnm))
+}
+
+// SetParams sets the params for "Base" and then current ParamSet.
+// If sheet is empty, then it applies all avail sheets (e.g., Network, Sim)
+// otherwise just the named sheet
+// if setMsg = true then we output a message for each param that was set.
+func SetParams(sheet string, setMsg bool, net *Network, params *params.Sets, paramName string, ss interface{}) error {
+	if sheet == "" {
+		// this is important for catching typos and ensuring that all sheets can be used
+		params.ValidateSheets([]string{"Network", "Sim"})
+	}
+	err := SetParamsSet("Base", sheet, setMsg, net, params, ss)
+	if paramName != "" && paramName != "Base" {
+		err = SetParamsSet(paramName, sheet, setMsg, net, params, ss)
+	}
+	return err
+}
+
+// SetParamsSet sets the params for given params.Set name.
+// If sheet is empty, then it applies all avail sheets (e.g., Network, Sim)
+// otherwise just the named sheet
+// if setMsg = true then we output a message for each param that was set.
+func SetParamsSet(setNm string, sheet string, setMsg bool, net *Network, params *params.Sets, ss interface{}) error {
+	pset, err := params.SetByNameTry(setNm)
+	if err != nil {
+		return err
+	}
+	if sheet == "" || sheet == "Network" {
+		netp, ok := pset.Sheets["Network"]
+		if ok {
+			net.ApplyParams(netp, setMsg)
+		}
+	}
+
+	if sheet == "" || sheet == "Sim" {
+		simp, ok := pset.Sheets["Sim"]
+		if ok {
+			simp.Apply(ss, setMsg)
+		}
+	}
+	// note: if you have more complex environments with parameters, definitely add
+	// sheets for them, e.g., "TrainEnv", "TestEnv" etc
+	return err
+}
+
+// HogDead computes the proportion of units in given layer name with ActAvg over hog thr and under dead threshold
+func HogDead(net *Network, lnm string) (hog, dead float64) {
+	ly := net.LayerByName(lnm).(AxonLayer).AsAxon()
+	n := len(ly.Neurons)
+	for ni := range ly.Neurons {
+		nrn := &ly.Neurons[ni]
+		if nrn.ActAvg > 0.3 {
+			hog += 1
+		} else if nrn.ActAvg < 0.01 {
+			dead += 1
+		}
+	}
+	hog /= float64(n)
+	dead /= float64(n)
+	return
+}
+
+// AddPlusAndMinusPhases adds the minus and plus phases of the theta cycle, which help the network learn.
+func AddPlusAndMinusPhases(manager *looper.Manager, time *Time, net *Network) (looper.Event, looper.Event, looper.Event) {
+	// The minus and plus phases of the theta cycle, which help the network learn.
+	minusPhase := looper.Event{Name: "MinusPhase", AtCtr: 0}
+	minusPhase.OnEvent.Add("Sim:MinusPhase:Start", func() {
+		time.PlusPhase = false
+		time.NewPhase(false)
+	})
+	plusPhase := looper.Event{Name: "PlusPhase", AtCtr: 150}
+	plusPhase.OnEvent.Add("Sim:MinusPhase:End", func() { net.MinusPhase(time) })
+	plusPhase.OnEvent.Add("Sim:PlusPhase:Start", func() {
+		time.PlusPhase = true
+		time.NewPhase(true)
+	})
+	plusPhaseEnd := looper.Event{Name: "PlusPhase", AtCtr: 199}
+	plusPhaseEnd.OnEvent.Add("Sim:PlusPhase:End", func() { net.PlusPhase(time) })
+	// Add both to train and test, by copy
+	manager.AddEventAllModes(etime.Cycle, minusPhase)
+	manager.AddEventAllModes(etime.Cycle, plusPhase)
+	manager.AddEventAllModes(etime.Cycle, plusPhaseEnd)
+	return minusPhase, plusPhase, plusPhaseEnd
+}
+
+// NewRndSeed gets a new random seed based on current time -- otherwise uses
+// the same random seed for every run.
+func NewRndSeed(randomSeed *int64) {
+	*randomSeed = time.Now().UnixNano()
+}
+
+// AddCommonLogItemsForOutputLayers helps the UserInterface add common logging items based on the output layers of an axon Network.
+func AddCommonLogItemsForOutputLayers(ui *egui.UserInterface) {
+	// Record time in logs
+	for m, st := range ui.Looper.Stacks {
+		mode := m
+		for t, l := range st.Loops {
+			time := t
+			loop := l
+			ui.Logs.AddItem(&elog.Item{
+				Name: t.String(),
+				Type: etensor.INT64,
+				Plot: elog.DFalse,
+				Write: elog.WriteMap{
+					etime.Scopes([]etime.Modes{mode}, []etime.Times{time}): func(ctx *elog.Context) {
+						ctx.SetInt(loop.Counter.Cur)
+					}}})
+		}
+	}
+
+	// Error for output layers
+	for _, olnm := range ui.Network.LayersByClass(emer.Target.String()) {
+		out := ui.Network.LayerByName(olnm).(AxonLayer).AsAxon()
+
+		cosDiffMap := elog.WriteMap{}
+		pctErrDiffMap := elog.WriteMap{}
+		trlErrDiffMap := elog.WriteMap{}
+		for m, st := range ui.Looper.Stacks {
+			for iter := len(st.Order) - 1; iter >= 0; iter-- {
+				i := iter // For closures
+				t := st.Order[i]
+				if i == len(st.Order)-1 {
+					// The finest timescale, such as Cycle
+					cosDiffMap[etime.Scope(m, t)] = func(ctx *elog.Context) {
+						ctx.SetFloat32(out.CosDiff.Cos)
+					}
+					pctErrDiffMap[etime.Scope(m, t)] = func(ctx *elog.Context) {
+						ctx.SetFloat64(out.PctUnitErr())
+					}
+					trlErrDiffMap[etime.Scope(m, t)] = func(ctx *elog.Context) {
+						pcterr := out.PctUnitErr()
+						trlerr := 0
+						if pcterr > 0 {
+							trlerr = 1
+						}
+						ctx.SetFloat64(float64(trlerr))
+					}
+				} else {
+					// All other, less frequent, timescales are an aggregate
+					for _, wm := range []elog.WriteMap{cosDiffMap, pctErrDiffMap, trlErrDiffMap} {
+						wm[etime.Scope(m, t)] = func(ctx *elog.Context) {
+							ctx.SetAgg(ctx.Mode, st.Order[i+1], agg.AggMean)
+						}
+					}
+				}
+			}
+		}
+
+		// Add it to the list.
+		ui.Logs.AddItem(&elog.Item{
+			Name:  "CosSim",
+			Type:  etensor.FLOAT64,
+			Plot:  elog.DTrue,
+			Write: cosDiffMap})
+		ui.Logs.AddItem(&elog.Item{
+			Name:  "PctErr",
+			Type:  etensor.FLOAT64,
+			Plot:  elog.DTrue,
+			Write: pctErrDiffMap})
+		ui.Logs.AddItem(&elog.Item{
+			Name:  "TrialErr",
+			Type:  etensor.FLOAT64,
+			Plot:  elog.DTrue,
+			Write: trlErrDiffMap})
+	}
+}
---
+++ examples/simple_agent/simple_agent.go
@@ -0,0 +1,122 @@
+// Copyright (c) 2022, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"github.com/emer/axon/axon"
+	"github.com/emer/axon/deep"
+	"github.com/emer/emergent/agent"
+	"github.com/emer/emergent/egui"
+	"github.com/emer/emergent/emer"
+	"github.com/emer/emergent/etime"
+	"github.com/emer/emergent/looper"
+	"github.com/emer/emergent/prjn"
+	"github.com/emer/etable/etensor"
+	"log"
+)
+
+// This file demonstrates the creation of a simple axon network connected to a simple world in a different file and potentially a different process. Although it is intended as a framework for creating an intelligent agent and embedding it in a challenging world, it does not actually implement any meaningful sort of intelligence, as the network receives no teaching signal of any kind. In addition to creating a simple environment and a simple network, it creates a looper.Manager to control the flow of time across Runs, Epochs, and Trials. It creates a GUI to control it.
+// Although this model does not learn or use a real environment, you may find it useful as a template for creating something more.
+
+func main() {
+	var sim Sim
+	sim.WorldEnv = sim.ConfigEnv()
+	sim.Net = sim.ConfigNet()
+	sim.Loops = sim.ConfigLoops()
+
+	userInterface := egui.UserInterface{
+		StructForView:             &sim,
+		Looper:                    sim.Loops,
+		Network:                   sim.Net.EmerNet,
+		AppName:                   "Agent",
+		AppTitle:                  "Simple Agent",
+		AppAbout:                  `A simple agent that can handle an arbitrary world.`,
+		AddNetworkLoggingCallback: axon.AddCommonLogItemsForOutputLayers,
+	}
+	userInterface.AddDefaultLogging()
+	userInterface.CreateAndRunGuiWithAdditionalConfig(func() {
+		sw, ok := sim.WorldEnv.(*agent.AgentProxyWithWorldCache) // See ConfigEnv()
+		if ok {
+			userInterface.AddServerButton(sw.GetServerFunc(sim.Loops))
+		}
+	}) // CreateAndRunGui blocks, so don't put any code after this.
+}
+
+// Sim encapsulates working data for the simulation model, keeping all relevant state information organized and available without having to pass everything around.
+type Sim struct {
+	Net      *deep.Network        `view:"no-inline" desc:"the network -- click to view / edit parameters for layers, prjns, etc"`
+	Loops    *looper.Manager      `view:"no-inline" desc:"contains looper control loops for running sim"`
+	WorldEnv agent.WorldInterface `desc:"Training environment -- contains everything about iterating over input / output patterns over training"`
+	Time     axon.Time            `desc:"axon timing parameters and state"`
+	LoopTime string               `desc:"Printout of the current time."`
+}
+
+func (ss *Sim) ConfigEnv() agent.WorldInterface {
+	return &agent.AgentProxyWithWorldCache{}
+}
+
+func (ss *Sim) ConfigNet() *deep.Network {
+	// A simple network for demonstration purposes.
+	net := &deep.Network{}
+	net.InitName(net, "Emery")
+	inp := net.AddLayer2D("Input", 5, 5, emer.Input)
+	hid1 := net.AddLayer2D("Hidden1", 10, 10, emer.Hidden)
+	hid2 := net.AddLayer2D("Hidden2", 10, 10, emer.Hidden)
+	out := net.AddLayer2D("Output", 5, 5, emer.Target)
+	full := prjn.NewFull()
+	net.ConnectLayers(inp, hid1, full, emer.Forward)
+	net.BidirConnectLayers(hid1, hid2, full)
+	net.BidirConnectLayers(hid2, out, full)
+
+	net.Defaults()
+	// see params_def.go for default params
+	axon.SetParams("Network", true, net.AsAxon(), &ParamSets, "", ss)
+	err := net.Build()
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return net
+}
+
+func (ss *Sim) NewRun() {
+	ss.Net.InitWts()
+}
+
+// ConfigLoops configures the control loops
+func (ss *Sim) ConfigLoops() *looper.Manager {
+	manager := looper.Manager{}.Init()
+	manager.Stacks[etime.Train] = &looper.Stack{}
+	manager.Stacks[etime.Train].Init().AddTime(etime.Run, 1).AddTime(etime.Epoch, 100).AddTime(etime.Trial, 100).AddTime(etime.Cycle, 200)
+
+	_, plusPhase, _ := axon.AddPlusAndMinusPhases(manager, &ss.Time, ss.Net.AsAxon())
+	plusPhase.OnEvent.Add("Sim:PlusPhase:SendActionsThenStep", func() {
+		// Check the action at the beginning of the Plus phase, before the teaching signal is introduced.
+		axon.SendActionAndStep(ss.Net.AsAxon(), ss.WorldEnv)
+	})
+
+	// Trial Stats and Apply Input
+	mode := etime.Train // For closures
+	stack := manager.Stacks[mode]
+	stack.Loops[etime.Trial].OnStart.Add("Sim:ResetState", func() {
+		ss.Net.NewState()
+		ss.Time.NewState(mode.String())
+	})
+	stack.Loops[etime.Trial].OnStart.Add("Sim:Trial:Observe", func() {
+		axon.ApplyInputs(ss.Net.AsAxon(), ss.WorldEnv, "Input", func(spec agent.SpaceSpec) etensor.Tensor {
+			// Use ObserveWithShape on the AgentProxyWithWorldCache which just returns a random vector of the correct size.
+			return ss.WorldEnv.(*agent.AgentProxyWithWorldCache).ObserveWithShape("Input", spec)
+		})
+	})
+
+	manager.GetLoop(etime.Train, etime.Run).OnStart.Add("Sim:NewRun", ss.NewRun)
+	axon.AddDefaultLoopSimLogic(manager, &ss.Time, ss.Net.AsAxon())
+
+	// Initialize and print loop structure, then add to Sim
+	manager.Init()
+	fmt.Println(manager.DocString())
+	return manager
+}
