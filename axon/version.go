62e7d00e1f2f9633dd90222018776d6c93559adf
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2022-06-13 01:52:34 -0700

    v1.4.9 release -- version.go updated

--- axon/version.go
+++ axon/version.go
@@ -3,7 +3,7 @@
 package axon
 
 const (
-	Version     = "v1.4.8"
-	GitCommit   = "ca06fe6"          // the commit JUST BEFORE the release
-	VersionDate = "2022-06-13 03:21" // UTC
+	Version     = "v1.4.9"
+	GitCommit   = "ea4a157"          // the commit JUST BEFORE the release
+	VersionDate = "2022-06-13 08:52" // UTC
 )
