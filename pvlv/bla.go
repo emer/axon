eec6d24d737cc49b47d4704202d40fa9ab93117f
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2022-10-10 15:04:09 -0700

    fix BLA prjn, add comments

--- pvlv/bla.go
+++ pvlv/bla.go
@@ -51,6 +51,10 @@ func (ly *BLALayer) PlusPhase(ltime *axon.Time) {
 //////////////////////////////////////////////////////////////////////////////////////
 //  BLAPrjn
 
+// TODO: switching to a minus-plus dynamic, not prior timestep -- so fix this
+// to use minus phase act only (test diff options).  Also, CtxtPrjn found that
+// rn.RLrate was problematic -- investigate for BLA
+
 // BLAPrjn does standard trace learning using phase differences modulated by DA,
 // and US modulation, using prior time sending activation to capture temporal
 // asymmetry in sending activity.
@@ -66,46 +70,6 @@ func (pj *BLAPrjn) Defaults() {
 	pj.SWt.Init.Mean = 0.1
 	pj.SWt.Init.Var = 0
 	pj.SWt.Init.Sym = false
-}
-
-// DWt computes the weight change (learning) -- on sending projections.
-func (pj *BLAPrjn) DWt(ltime *axon.Time) {
-	if !pj.Learn.Learn {
-		return
-	}
-	kp := &pj.Learn.KinaseCa
-	slay := pj.Send.(axon.AxonLayer).AsAxon()
-	rlay := pj.Recv.(axon.AxonLayer).AsAxon()
-	ctime := int32(ltime.CycleTot)
-	lr := pj.Learn.Lrate.Eff
-	for si := range slay.Neurons {
-		sn := &slay.Neurons[si]
-		nc := int(pj.SConN[si])
-		st := int(pj.SConIdxSt[si])
-		syns := pj.Syns[st : st+nc]
-		scons := pj.SConIdx[st : st+nc]
-		for ci := range syns {
-			ri := scons[ci]
-			rn := &rlay.Neurons[ri]
-			sy := &syns[ci]
-			kp.CurCa(ctime, sy.CaUpT, sy.CaM, sy.CaP, sy.CaD) // always update
-			// only difference from standard is that Tr updates *after* DWt instead of before!
-			// note: CaSpkP - CaSpkD works MUCH better than plain Ca
-			err := sy.Tr * (rn.CaSpkP - rn.CaSpkD)
-			sy.Tr = pj.Learn.Trace.TrFmCa(sy.Tr, sn.CaD) // caD is better: reflects entire window
-			if sy.Wt == 0 {                              // failed con, no learn
-				continue
-			}
-			// note: trace ensures that nothing changes for inactive synapses..
-			// sb immediately -- enters into zero sum
-			if err > 0 {
-				err *= (1 - sy.LWt)
-			} else {
-				err *= sy.LWt
-			}
-			sy.DWt += rn.RLrate * lr * err
-		}
-	}
 }
 
 // WtFmDWt updates the synaptic weight values from delta-weight changes -- on sending projections
