503fc29be3b7b33d587994f485895426e39fffe9
Erdal <etuleu@gmail.com>
2022-11-08 13:09:58 -0800

    Save the file every epoch

--- examples/bench/bench.go
+++ examples/bench/bench.go
@@ -25,10 +25,12 @@ import (
 	"github.com/emer/emergent/timer"
 	"github.com/emer/etable/etable"
 	"github.com/emer/etable/etensor"
+	"github.com/goki/gi/gi"
 )
 
 var Net *axon.Network
 var Pats *etable.Table
+var Filename string
 var EpcLog *etable.Table
 var Thread = false // much slower for small net
 var Silent = false // non-verbose mode -- just reports result
@@ -123,12 +125,12 @@ func ConfigEpcLog(dt *etable.Table) {
 		{"CorSim", etensor.FLOAT32, nil, nil},
 		{"AvgCorSim", etensor.FLOAT32, nil, nil},
 		{"SSE", etensor.FLOAT32, nil, nil},
-		{"Count Err", etensor.FLOAT32, nil, nil},
-		{"Pct Err", etensor.FLOAT32, nil, nil},
-		{"Pct Cor", etensor.FLOAT32, nil, nil},
-		{"Hid1 ActAvg", etensor.FLOAT32, nil, nil},
-		{"Hid2 ActAvg", etensor.FLOAT32, nil, nil},
-		{"Out ActAvg", etensor.FLOAT32, nil, nil},
+		{"CountErr", etensor.FLOAT32, nil, nil},
+		{"PctErr", etensor.FLOAT32, nil, nil},
+		{"PctCor", etensor.FLOAT32, nil, nil},
+		{"Hid1ActAvg", etensor.FLOAT32, nil, nil},
+		{"Hid2ActAvg", etensor.FLOAT32, nil, nil},
+		{"OutActAvg", etensor.FLOAT32, nil, nil},
 	}, 0)
 }
 
@@ -194,17 +196,23 @@ func TrainNet(net *axon.Network, pats, epcLog *etable.Table, epcs int) {
 		sse /= float64(np)
 		pctErr := float64(cntErr) / float64(np)
 		pctCor := 1 - pctErr
-		// fmt.Printf("epc: %v  \tCosDiff: %v \tAvgCosDif: %v\n", epc, outCorSim, outLay.CosDiff.Avg)
+
+		t := tmr.Stop()
+		tmr.Start()
+		fmt.Printf("epc: %v  \tCorSim: %v \tAvgCorSim: %v \tTime:%v\n", epc, outCorSim, outLay.CorSim.Avg, t)
+
 		epcLog.SetCellFloat("Epoch", epc, float64(epc))
 		epcLog.SetCellFloat("CorSim", epc, float64(outCorSim))
 		epcLog.SetCellFloat("AvgCorSim", epc, float64(outLay.CorSim.Avg))
 		epcLog.SetCellFloat("SSE", epc, sse)
-		epcLog.SetCellFloat("Count Err", epc, float64(cntErr))
-		epcLog.SetCellFloat("Pct Err", epc, pctErr)
-		epcLog.SetCellFloat("Pct Cor", epc, pctCor)
-		epcLog.SetCellFloat("Hid1 ActAvg", epc, float64(hid1Lay.ActAvg.ActMAvg))
-		epcLog.SetCellFloat("Hid2 ActAvg", epc, float64(hid2Lay.ActAvg.ActMAvg))
-		epcLog.SetCellFloat("Out ActAvg", epc, float64(outLay.ActAvg.ActMAvg))
+		epcLog.SetCellFloat("CountErr", epc, float64(cntErr))
+		epcLog.SetCellFloat("PctErr", epc, pctErr)
+		epcLog.SetCellFloat("PctCor", epc, pctCor)
+		epcLog.SetCellFloat("Hid1ActAvg", epc, float64(hid1Lay.ActAvg.ActMAvg))
+		epcLog.SetCellFloat("Hid2ActAvg", epc, float64(hid2Lay.ActAvg.ActMAvg))
+		epcLog.SetCellFloat("OutActAvg", epc, float64(outLay.ActAvg.ActMAvg))
+
+		EpcLog.SaveCSV(gi.FileName(Filename), ',', etable.Headers)
 	}
 	tmr.Stop()
 	if Silent {
@@ -247,7 +255,9 @@ func main() {
 	EpcLog = &etable.Table{}
 	ConfigEpcLog(EpcLog)
 
+	Filename = fmt.Sprintf("bench_%d_units.csv", units)
+
 	TrainNet(Net, Pats, EpcLog, epochs)
 
-	EpcLog.SaveCSV("bench_epc.dat", ',', etable.Headers)
+	EpcLog.SaveCSV(gi.FileName(Filename), ',', etable.Headers)
 }
