66853d3fc4f8d3f7a6422720a39d7cc3ed8cd8ba
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2023-02-08 15:44:15 -0800

    run ginormus and huge in gpu bench

--- examples/bench/run_gpu.sh
+++ examples/bench/run_gpu.sh
@@ -40,14 +40,16 @@ CMD=(${exe} -test.bench=BenchmarkBenchNetFull -writestats)
 echo " "
 echo "=============================================================="
 echo "HUGE Network (5 x 1024 units)"
-# turning off verbose allows fast cycle-level processing
+turning off verbose allows fast cycle-level processing
 ${CMD[@]} -epochs 2 -pats 5 -units 1024 -gpu -verbose=false $*
 ${CMD[@]} -epochs 2 -pats 5 -units 1024 -gpu $*
 ${CMD[@]} -epochs 2 -pats 5 -units 1024 $*
-# echo " "
-# echo "=============================================================="
-# echo "GINORMOUS Network (5 x 2048 units)"
-# ${CMD[@]} -epochs 1 -pats 1 -units 2048 $*
+echo " "
+echo "=============================================================="
+echo "GINORMOUS Network (5 x 2048 units)"
+${CMD[@]} -epochs 1 -pats 5 -units 2048 -gpu -verbose=false $*
+${CMD[@]} -epochs 1 -pats 5 -units 2048 -gpu $*
+${CMD[@]} -epochs 1 -pats 5 -units 2048 $*
 # echo " "
 # echo "=============================================================="
 # echo "GAZILIOUS Network (5 x 4096 units)"
