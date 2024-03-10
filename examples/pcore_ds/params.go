a0002516c1ab487bb6d26b51c9360d9ec33ede2c
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-03-10 10:49:13 -0700

    ds update params to mahp .01, sahp .05 std

--- examples/pcore_ds/params.go
+++ examples/pcore_ds/params.go
@@ -24,28 +24,28 @@ var ParamSets = netparams.Sets{
 			}},
 		{Sel: ".DBG", Desc: "all bg",
 			Params: params.Params{
-				"Layer.Acts.Mahp.Gbar": "0.0",
+				"Layer.Acts.Mahp.Gbar": "0.01",
 				"Layer.Acts.Sahp.Gbar": "0.05", // note: Pr getting lots of Sahp
 			},
 			Hypers: params.Hypers{
-				"Layer.Acts.Mahp.Gbar": {"Tweak": "-"},
-				"Layer.Acts.Sahp.Gbar": {"Tweak": "-"},
+				"Layer.Acts.Mahp.Gbar": {"Tweak": "[0.01,0.015,0.02]"},
+				"Layer.Acts.Sahp.Gbar": {"Tweak": "[0,0.03]"},
 			}},
 		{Sel: ".MatrixLayer", Desc: "all mtx",
 			Params: params.Params{
-				"Layer.Inhib.Pool.Gi":            "0.5",   // 0.5 > others
-				"Layer.Learn.NeuroMod.BurstGain": "0.1",   // 0.1 == 0.2 > 0.05 > 0.5 -- key lrate modulator
-				"Layer.Learn.RLRate.On":          "true",  // note: applied for tr update trials
-				"Layer.Learn.TrgAvgAct.On":       "true",  // true > false
-				"Layer.Acts.Mahp.Gbar":           "0.015", // 0.01 > 0.02 > 0
-				"Layer.Acts.Sahp.Gbar":           "0.03",  // todo test
+				"Layer.Inhib.Pool.Gi":            "0.5",  // 0.5 > others
+				"Layer.Learn.NeuroMod.BurstGain": "0.1",  // 0.1 == 0.2 > 0.05 > 0.5 -- key lrate modulator
+				"Layer.Learn.RLRate.On":          "true", // note: applied for tr update trials
+				"Layer.Learn.TrgAvgAct.On":       "true", // true > false
+				"Layer.Acts.Mahp.Gbar":           "0.01", // 0.01 > 0.02 > 0
+				"Layer.Acts.Sahp.Gbar":           "0.05", // todo test
 			},
 			Hypers: params.Hypers{
 				"Layer.Learn.NeuroMod.BurstGain": {"Tweak": "-"},
 				"Layer.Acts.Kir.Gbar":            {"Tweak": "-"},
 				"Layer.Inhib.Pool.Gi":            {"Tweak": "-"},
-				"Layer.Acts.Mahp.Gbar":           {"Tweak": "-"},
-				"Layer.Acts.Sahp.Gbar":           {"Tweak": "-"},
+				"Layer.Acts.Mahp.Gbar":           {"Tweak": "[0,0.01,0.02]"},
+				"Layer.Acts.Sahp.Gbar":           {"Tweak": "[0,0.05]"},
 			}},
 		{Sel: ".DSTNLayer", Desc: "all STN",
 			Params: params.Params{
@@ -71,8 +71,8 @@ var ParamSets = netparams.Sets{
 				"Layer.Acts.Sahp.Gbar": "0.05", // 0.05 def
 			},
 			Hypers: params.Hypers{
-				"Layer.Acts.Mahp.Gbar": {"Tweak": "[0.01,0.015,0.005]"},
-				"Layer.Acts.Sahp.Gbar": {"Tweak": "[0.04,0.03,0.02]"},
+				"Layer.Acts.Mahp.Gbar": {"Tweak": "[0,0.01,0.015]"},
+				"Layer.Acts.Sahp.Gbar": {"Tweak": "[0,0.03]"},
 			}},
 		{Sel: ".PTMaintLayer", Desc: "time integration params",
 			Params: params.Params{
@@ -80,21 +80,21 @@ var ParamSets = netparams.Sets{
 				"Layer.Acts.Dend.ModGain":   "1.0",   // 1.5 def
 				"Layer.Acts.Kir.Gbar":       "0",     // no real diff here over range 0-10
 				"Layer.Acts.MaintNMDA.Gbar": "0.007", // 0.007 default
-				"Layer.Acts.Mahp.Gbar":      "0",     // 0.02 def
+				"Layer.Acts.Mahp.Gbar":      "0.01",  // 0.02 def
 				"Layer.Acts.Sahp.Gbar":      "0.05",  // 0.05 def
 			},
 			Hypers: params.Hypers{
-				"Layer.Acts.Mahp.Gbar": {"Tweak": "[0.01,0.015,0.005]"},
-				"Layer.Acts.Sahp.Gbar": {"Tweak": "[0.04,0.03,0.02]"},
+				"Layer.Acts.Mahp.Gbar": {"Tweak": "[0.01,0.015,0.02]"},
+				"Layer.Acts.Sahp.Gbar": {"Tweak": "[0,0.03]"},
 			}},
 		{Sel: ".PTPredLayer", Desc: "",
 			Params: params.Params{
-				"Layer.Acts.Mahp.Gbar": "0",    // 0.02 def
+				"Layer.Acts.Mahp.Gbar": "0.01", // 0.02 def
 				"Layer.Acts.Sahp.Gbar": "0.05", // 0.05 def
 			},
 			Hypers: params.Hypers{
-				"Layer.Acts.Mahp.Gbar": {"Tweak": "[0.01,0.015,0.005]"},
-				"Layer.Acts.Sahp.Gbar": {"Tweak": "[0.04,0.03,0.02]"},
+				"Layer.Acts.Mahp.Gbar": {"Tweak": "[0.01,0.015,0.02]"},
+				"Layer.Acts.Sahp.Gbar": {"Tweak": "[0,0.03]"},
 			}},
 		{Sel: "#MotorBS", Desc: "",
 			Params: params.Params{
