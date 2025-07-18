b356366c1699052f2574d59ffa4d3494f6b51656
Kai O'Reilly <koreilly5297@gmail.com>
2025-07-18 14:35:35 -0700

    deepmove: update to new egui helpers

--- sims/deepmove/config.go
+++ sims/deepmove/config.go
@@ -5,8 +5,7 @@
 package deepmove
 
 import (
-	"cogentcore.org/core/base/errors"
-	"cogentcore.org/core/base/reflectx"
+	"github.com/emer/emergent/v2/egui"
 )
 
 // EnvConfig has config params for environment
@@ -59,10 +58,6 @@ type ParamConfig struct { //types:add
 
 // RunConfig has config parameters related to running the sim.
 type RunConfig struct { //types:add
-
-	// GPU uses the GPU for computation, generally faster than CPU even for
-	// small models if NData ~16.
-	GPU bool `default:"true"`
 
 	// GPUDevice selects the gpu device to use.
 	GPUDevice int
@@ -124,29 +119,7 @@ type LogConfig struct {
 
 // Config has the overall Sim configuration options.
 type Config struct {
-
-	// Name is the short name of the sim.
-	Name string `display:"-" default:"DeepMove"`
-
-	// Title is the longer title of the sim.
-	Title string `display:"-" default:"DeepAxon Move Prediction"`
-
-	// URL is a link to the online README or other documentation for this sim.
-	URL string `display:"-" default:"https://github.com/emer/axon/blob/main/sims/deep_move/README.md"`
-
-	// Doc is brief documentation of the sim.
-	Doc string `display:"-" default:"This demonstrates a basic deep predictive learning Axon model that learns to predict the effects of movement on visual inputs."`
-
-	// Includes has a list of additional config files to include.
-	// After configuration, it contains list of include files added.
-	Includes []string
-
-	// GUI means open the GUI. Otherwise it runs automatically and quits,
-	// saving results to log files.
-	GUI bool `default:"true"`
-
-	// Debug reports debugging information.
-	Debug bool
+	egui.BaseConfig
 
 	// Env has environment related configuration options.
 	Env EnvConfig `display:"add-fields"`
@@ -161,14 +134,9 @@ type Config struct {
 	Log LogConfig `display:"add-fields"`
 }
 
-func (cfg *Config) IncludesPtr() *[]string { return &cfg.Includes }
-
 func (cfg *Config) Defaults() {
-	errors.Log(reflectx.SetFromDefaultTags(cfg))
-}
-
-func NewConfig() *Config {
-	cfg := &Config{}
-	cfg.Defaults()
-	return cfg
+	cfg.Name = "DeepMove"
+	cfg.Title = "DeepAxon Move Prediction"
+	cfg.URL = "https://github.com/emer/axon/blob/main/sims/deepmove/README.md"
+	cfg.Doc = "This demonstrates a basic deep predictive learning Axon model that learns to predict the effects of movement on visual inputs."
 }
